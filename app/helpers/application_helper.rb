1f5c1a25d16236d51d7df5ff63e9e0c5d9483a4d
yuta <yu.code.tech@gmail.com>
2025-09-17 23:36:08 +0900

    refactor: flash の実装を改善

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_accent_class(key)
+    case key.to_s
+    when 'success', 'notice'
+      'bg-emerald-500'
+    when 'error', 'alert'
+      'bg-red-500'
+    else
+      'bg-slate-500'
+    end
+  end
 end
