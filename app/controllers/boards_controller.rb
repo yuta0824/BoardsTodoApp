0f010c384b39030c564c8f2b899844ef64e6970d
yuta <yu.code.tech@gmail.com>
2025-09-24 22:18:25 +0900

    feat: edit/update の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -15,7 +15,7 @@ class BoardsController < ApplicationController
     if @board.save
       redirect_to board_path(@board), notice: 'ボードが作成されました'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -24,9 +24,17 @@ class BoardsController < ApplicationController
   end
 
   def edit
+    @board = current_user.boards.find(params[:id])
   end
 
   def update
+    @board = current_user.boards.find(params[:id])
+
+    if @board.update(board_params)
+      redirect_to board_path(@board), notice: 'ボードが更新されました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
