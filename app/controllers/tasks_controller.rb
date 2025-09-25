633ab702e9880122a66e9de317c1275a1e24abe6
Yuta | CodeYu <yu.code.tech@gmail.com>
2025-09-26 01:02:12 +0900

    feat: #14 タスク機能_投稿機能の実装 (#20)

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+    before_action :authenticate_user!, except: [:show]
+
+    def new
+        @board = current_user.boards.find(params[:board_id])
+        @task = @board.tasks.build
+    end
+
+    def create
+        @board = current_user.boards.find(params[:board_id])
+        @task = current_user.tasks.build(task_params.merge(board: @board))
+
+        if @task.save
+            redirect_to board_task_path(@board.id, @task.id), notice: 'タスクが作成されました'
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @board = Board.find(params[:board_id])
+        @task = @board.tasks.find(params[:id])
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:name, :description, :due_date)
+    end
+
+end
