d2aef82e6ba1c1b965764c329cd09fc8532fc16a
Yuta | CodeYu <yu.code.tech@gmail.com>
2025-09-27 02:13:19 +0900

    feat: #24 task に thumbnail を追加 (#25)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,7 +25,7 @@ class TasksController < ApplicationController
     private
 
     def task_params
-        params.require(:task).permit(:name, :description, :due_date)
+        params.require(:task).permit(:name, :description, :due_date, :thumbnail)
     end
 
 end
