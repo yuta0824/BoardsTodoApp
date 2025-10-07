5a013e3caf2d88c6454f749e24c021c2e315a1a0
yuta <yu.code.tech@gmail.com>
2025-10-07 23:36:24 +0900

    feat: dotenv インストール

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module BoardsTodoApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
+    Bundler.require(*Rails.groups)
+    Dotenv::Rails.logger
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
