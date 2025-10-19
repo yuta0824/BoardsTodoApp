2f40a0d4705ca532841d5bc9077dc5e6bfba6df3
yuta <yu.code.tech@gmail.com>
2025-10-19 21:52:18 +0900

    refactor: bin/lint

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -23,8 +23,8 @@ class BoardsController < ApplicationController
   def show
     @board = Board.find(params[:id])
     @tasks = @board.tasks.order(created_at: :asc)
-    @done_tasks = @tasks.where(status: "done")
-    @todo_tasks = @tasks.where(status: "todo")
+    @done_tasks = @tasks.where(status: 'done')
+    @todo_tasks = @tasks.where(status: 'todo')
   end
 
   def edit
