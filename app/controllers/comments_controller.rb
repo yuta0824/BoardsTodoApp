f158c2ff995e02e321443700cd44cedd254d2368
yuta <yu.code.tech@gmail.com>
2025-10-20 22:57:32 +0900

    refactor: CommentsController のコードを整理

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,15 @@
 class CommentsController < ApplicationController
-    before_action :authenticate_user!, except: [:index]
-    before_action :set_board_and_task, only: [:index, :new, :create]
+    before_action :authenticate_user!, only: [:create, :destroy]
+    before_action :set_board_and_task, only: [:index, :create]
 
     def index
       render_comments(@task)
     end
 
-    def new
-      @comment = current_user.comments.build
-    end
-
     def create
       comment = current_user.comments.build(comment_params.merge(task: @task))
-      task = comment.task
       comment.save!
-      render_comments(task)
+      render_comments(@task)
     end
 
     def destroy
