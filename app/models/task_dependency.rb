3ec23065c958a12dd987f96cc4994d99eb614af0
yuta <yu.code.tech@gmail.com>
2025-10-22 00:58:53 +0900

    feat: 自己依存・循環防止のバリデーションを追加

--- app/models/task_dependency.rb
+++ app/models/task_dependency.rb
@@ -1,4 +1,22 @@
 class TaskDependency < ApplicationRecord
   belongs_to :predecessor, class_name: 'Task'
   belongs_to :successor, class_name: 'Task'
+
+  validates :predecessor_id, uniqueness: { scope: :successor_id }
+  validate :dependent_self
+  validate :prevent_circular_dependency
+
+  private
+
+  def dependent_self
+    if predecessor_id == successor_id
+      errors.add(:base, "タスクは自分自身に依存できません")
+    end
+  end
+
+  def prevent_circular_dependency
+    if TaskDependency.exists?(predecessor_id: successor_id)
+      errors.add(:base, "タスクは循環依存できません")
+    end
+  end
 end
