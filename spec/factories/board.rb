f75e25c915aa449e5742ed7d981617de27eb6e93
yuta <yu.code.tech@gmail.com>
2025-10-27 02:03:01 +0900

    test: Boardファクトリに association :user を追加

--- spec/factories/board.rb
+++ spec/factories/board.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :board do
     name { Faker::Lorem.characters(number: 10) }
     description { Faker::Lorem.characters(number: 30) }
+    association :user
   end
 end
