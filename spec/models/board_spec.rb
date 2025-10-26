892d375d3b50fe00b4ebd8760ae6e224f7a66c3f
yuta <yu.code.tech@gmail.com>
2025-10-26 23:39:49 +0900

    test: context を追加

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
-  it "タイトルが入力されていれば、ボードを保存できる" do
-    user = User.create!(
-      name: Faker::Name.name,
-      email: Faker::Internet.email,
-      password: Faker::Lorem.characters(number: 10),
-    )
-    board = user.boards.build(
-      name: Faker::Lorem.characters(number: 10),
-      description: Faker::Lorem.characters(number: 300)
-    )
+  context 'タイトルが入力されている場合' do
+    before do
+      user = User.create!(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: Faker::Lorem.characters(number: 10),
+      )
+      @board = user.boards.build(
+        name: Faker::Lorem.characters(number: 10),
+        description: Faker::Lorem.characters(number: 300)
+      )
+    end
 
-    expect(board).to be_valid
+    it "ボードを保存できる" do
+      expect(@board).to be_valid
+    end
   end
 end
