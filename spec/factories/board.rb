5797a5b9fa3bb15f1f66442ec8bae36b4644f522
yuta <yu.code.tech@gmail.com>
2025-10-27 01:47:59 +0900

    test: FactoryBot に board を追加

---
+++ spec/factories/board.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :board do
+    name { Faker::Lorem.characters(number: 10) }
+    description { Faker::Lorem.characters(number: 30) }
+  end
+end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
-  let!(:user) { create(:user, email: "test@test.com") }
+  let!(:user) { create(:user) }
 
   context 'タイトルが入力されている場合' do
-    let!(:board) do
-      user.boards.build(
-        name: Faker::Lorem.characters(number: 10),
-        description: Faker::Lorem.characters(number: 300)
-      )
-    end
+    let!(:board) { build(:board, user: user) }
 
     it "ボードを保存できる" do
       expect(board).to be_valid
@@ -17,12 +12,7 @@ RSpec.describe Board, type: :model do
   end
 
   context 'タイトルが入力されていない場合' do
-    let!(:board) do
-      user.boards.create(
-        name: "",
-        description: Faker::Lorem.characters(number: 300)
-      )
-    end
+    let!(:board) { build(:board, name: "", user: user) }
 
     it "ボードを保存できない" do
       expect(board).not_to be_valid
