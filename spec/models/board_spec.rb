a12f62c45e503bf16da1263efbccc9e63543d0cf
yuta <yu.code.tech@gmail.com>
2025-10-26 23:54:11 +0900

    test: ボードを保存できない場合をテスト

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
+  let!(:user) do
+    User.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: Faker::Lorem.characters(number: 10),
+    )
+  end
+
   context 'タイトルが入力されている場合' do
-    let!(:user) do
-      User.create!(
-        name: Faker::Name.name,
-        email: Faker::Internet.email,
-        password: Faker::Lorem.characters(number: 10),
-      )
-    end
     let!(:board) do
       user.boards.build(
         name: Faker::Lorem.characters(number: 10),
@@ -18,6 +19,20 @@ RSpec.describe Board, type: :model do
 
     it "ボードを保存できる" do
       expect(board).to be_valid
+    end
+  end
+
+  context 'タイトルが入力されていない場合' do
+    let!(:board) do
+      user.boards.create(
+        name: "",
+        description: Faker::Lorem.characters(number: 300)
+      )
+    end
+
+    it "ボードを保存できない" do
+      expect(board).not_to be_valid
+      expect(board.errors.details[:name]).to include({ error: :blank })
     end
   end
 end
