c91062ea7c353c77a4960cf685cbbac38470e913
yuta <yu.code.tech@gmail.com>
2025-10-26 23:43:06 +0900

    tes: let! を使って書き換える

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -2,20 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Board, type: :model do
   context 'タイトルが入力されている場合' do
-    before do
-      user = User.create!(
+    let!(:user) do
+      User.create!(
         name: Faker::Name.name,
         email: Faker::Internet.email,
         password: Faker::Lorem.characters(number: 10),
       )
-      @board = user.boards.build(
+    end
+    let!(:board) do
+      user.boards.build(
         name: Faker::Lorem.characters(number: 10),
         description: Faker::Lorem.characters(number: 300)
       )
     end
 
     it "ボードを保存できる" do
-      expect(@board).to be_valid
+      expect(board).to be_valid
     end
   end
 end
