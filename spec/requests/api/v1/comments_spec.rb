da9e551159131412e2fa455c114a3da69768f955
yuta <yu.code.tech@gmail.com>
2025-11-01 13:23:05 +0900

    refactor: bin/lint 実行

--- spec/requests/api/v1/comments_spec.rb
+++ spec/requests/api/v1/comments_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Comments", type: :request do
+RSpec.describe 'Api::V1::Comments', type: :request do
   let!(:user) { create(:user) }
   let!(:board) { create(:board, user: user) }
   let!(:task) { create(:task, user: user, board: board) }
   let!(:comments) { create_list(:comment, 3, user: user, task: task) }
 
-  describe "GET /api/v1/comments" do
-    context "ログインしている場合" do
+  describe 'GET /api/v1/comments' do
+    context 'ログインしている場合' do
       before do
         sign_in user
       end
 
-      it "200 Status" do
+      it '200 Status' do
         get api_v1_board_task_comments_path(board_id: board.id, task_id: task.id)
         expect(response).to have_http_status(200)
       end
     end
   end
 
-  describe "POST /api/v1/comments" do
-    context "ログインしている場合" do
+  describe 'POST /api/v1/comments' do
+    context 'ログインしている場合' do
       before do
         sign_in user
       end
 
-      it "コメントが保存される" do
+      it 'コメントが保存される' do
         comment_params = attributes_for(:comment)
         post api_v1_board_task_comments_path(board_id: board.id, task_id: task.id), params: {comment: comment_params}
         expect(response).to have_http_status(200)
@@ -34,13 +34,13 @@ RSpec.describe "Api::V1::Comments", type: :request do
     end
   end
 
-  describe "DELETE /api/v1/comments" do
-    context "ログインしている場合" do
+  describe 'DELETE /api/v1/comments' do
+    context 'ログインしている場合' do
       before do
         sign_in user
       end
 
-      it "コメントが削除される" do
+      it 'コメントが削除される' do
         comment = comments.last
         delete api_v1_comment_path(id: comment.id)
         expect(response).to have_http_status(200)
--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Boards", type: :request do
+RSpec.describe 'Boards', type: :request do
   let!(:user) { create(:user) }
   let!(:boards) { create_list(:board, 3, user: user) }
 
-  describe "GET /boards" do
-    it "200ステータスが返ってくる" do
+  describe 'GET /boards' do
+    it '200ステータスが返ってくる' do
       get boards_path
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "POST /boards" do
-    context "ログインしている場合" do
+  describe 'POST /boards' do
+    context 'ログインしている場合' do
       before do
         sign_in user
       end
 
-      it "ボードが保存される" do
+      it 'ボードが保存される' do
         board_params = attributes_for(:board)
         post boards_path, params: {board: board_params}
         expect(response).to have_http_status(302)
@@ -27,8 +27,8 @@ RSpec.describe "Boards", type: :request do
     end
   end
 
-  context "ログインしていない場合" do
-    it "ログイン画面に遷移する" do
+  context 'ログインしていない場合' do
+    it 'ログイン画面に遷移する' do
       board_params = attributes_for(:board)
       post boards_path, params: {board: board_params}
       expect(response).to redirect_to(new_user_session_path)
