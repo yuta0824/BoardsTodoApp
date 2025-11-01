c4a509836f93b367a534659d8c73352a3509f15a
Yuta | CodeYu <yu.code.tech@gmail.com>
2025-11-01 16:13:00 +0900

    test: #59 System Spec テスト実装 (#65)
    * test: 基本のテストを実行
    
    * test: idを使うことで、テストの精度を高める
    
    * test: ボードクリック時の詳細ページをテスト

---
+++ spec/system/board_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Board', type: :system do
+  let!(:user) { create(:user) }
+  let!(:boards) { create_list(:board, 3, user: user) }
+
+  it 'ボード一覧が表示される' do
+    visit root_path
+
+    boards.each do |board|
+      expect(page).to have_selector("[data-id='#{board.id}']", text: board.name)
+    end
+  end
+
+  it 'ボードの詳細が表示される' do
+    visit root_path
+    board = boards.first
+    board_card = find("[data-id='#{board.id}'] a")
+    board_card.click
+    expect(page).to have_selector("[data-id='#{board.id}']", text: board.name)
+  end
+end
