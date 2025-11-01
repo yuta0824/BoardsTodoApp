26aa10a6b32c8704a466e0b78d8ff76d45523aa0
yuta <yu.code.tech@gmail.com>
2025-11-01 17:31:24 +0900

    test: Comment API の rswag を生成

--- spec/requests/api/v1/comments_spec.rb
+++ spec/requests/api/v1/comments_spec.rb
@@ -1,50 +1,121 @@
 require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe 'Api::V1::Comments', type: :request do
+COMMENT_COLLECTION_SCHEMA = {
+  type: :array,
+  items: {
+    type: :object,
+    properties: {
+      id: { type: :integer },
+      content: { type: :string },
+      commenterName: { type: :string },
+      commenterAvatar: { type: :string },
+      isOwner: { type: :boolean }
+    },
+    required: %w[id content commenterName commenterAvatar isOwner]
+  }
+}.freeze
+
+RSpec.describe 'Comments API', type: :request do
   let!(:user) { create(:user) }
   let!(:board) { create(:board, user: user) }
   let!(:task) { create(:task, user: user, board: board) }
-  let!(:comments) { create_list(:comment, 3, user: user, task: task) }
 
-  describe 'GET /api/v1/comments' do
-    context 'ログインしている場合' do
-      before do
-        sign_in user
+  path '/api/v1/boards/{board_id}/tasks/{task_id}/comments' do
+    parameter name: :board_id, in: :path, type: :integer, description: 'Board ID'
+    parameter name: :task_id, in: :path, type: :integer, description: 'Task ID'
+
+    get 'コメント一覧を表示する' do
+      tags 'Comments'
+      produces 'application/json'
+
+      let!(:comments) { create_list(:comment, 3, user: user, task: task) }
+      let(:board_id) { board.id }
+      let(:task_id) { task.id }
+
+      before { sign_in user }
+
+      response '200', 'コメント一覧を取得' do
+        schema COMMENT_COLLECTION_SCHEMA
+
+        run_test! do |response|
+          body = JSON.parse(response.body)
+          expect(body.size).to eq(comments.size)
+          first_comment = body.first
+
+          expect(first_comment.keys).to match_array(%w[id content commenterName commenterAvatar isOwner])
+          expect(first_comment['commenterName']).to eq(user.name)
+          expect(first_comment['commenterAvatar']).to be_a(String)
+          expect(first_comment['commenterAvatar']).to be_present
+          expect(first_comment['isOwner']).to eq(true)
+        end
       end
+    end
 
-      it '200 Status' do
-        get api_v1_board_task_comments_path(board_id: board.id, task_id: task.id)
-        expect(response).to have_http_status(200)
+    post 'コメントを作成する' do
+      tags 'Comments'
+      consumes 'application/json'
+      produces 'application/json'
+
+      parameter name: :comment, in: :body, required: true, schema: {
+        type: :object,
+        properties: {
+          comment: {
+            type: :object,
+            properties: {
+              content: { type: :string }
+            },
+            required: %w[content]
+          }
+        },
+        required: %w[comment]
+      }
+
+      let(:board_id) { board.id }
+      let(:task_id) { task.id }
+      let(:comment) { { comment: { content: 'New comment from rswag' } } }
+
+      before { sign_in user }
+
+      response '200', 'コメントが作成される' do
+        schema COMMENT_COLLECTION_SCHEMA
+
+        run_test! do |_response|
+          body = JSON.parse(response.body)
+          created_comment = body.last
+
+          expect(created_comment['content']).to eq('New comment from rswag')
+          expect(created_comment['commenterName']).to eq(user.name)
+          expect(created_comment['isOwner']).to eq(true)
+          expect(created_comment['id']).to eq(Comment.last.id)
+        end
       end
     end
   end
 
-  describe 'POST /api/v1/comments' do
-    context 'ログインしている場合' do
-      before do
-        sign_in user
-      end
+  path '/api/v1/comments/{id}' do
+    parameter name: :id, in: :path, type: :integer, description: 'Comment ID'
 
-      it 'コメントが保存される' do
-        comment_params = attributes_for(:comment)
-        post api_v1_board_task_comments_path(board_id: board.id, task_id: task.id), params: {comment: comment_params}
-        expect(response).to have_http_status(200)
-        expect(Comment.last.content).to eq(comment_params[:content])
-      end
-    end
-  end
+    delete 'コメントを削除する' do
+      tags 'Comments'
+      produces 'application/json'
 
-  describe 'DELETE /api/v1/comments' do
-    context 'ログインしている場合' do
-      before do
-        sign_in user
-      end
+      let!(:comments) { create_list(:comment, 2, user: user, task: task) }
+      let(:comment_to_delete) { comments.first }
+      let(:id) { comment_to_delete.id }
 
-      it 'コメントが削除される' do
-        comment = comments.last
-        delete api_v1_comment_path(id: comment.id)
-        expect(response).to have_http_status(200)
-        expect(Comment.exists?(comment.id)).to be_falsey
+      before { sign_in user }
+
+      response '200', 'コメントが削除される' do
+        schema COMMENT_COLLECTION_SCHEMA
+
+        run_test! do |_response|
+          body = JSON.parse(response.body)
+          expect(Comment.exists?(comment_to_delete.id)).to be_falsey
+          remaining_ids = body.map { |comment| comment['id'] }
+          expect(remaining_ids).not_to include(comment_to_delete.id)
+          expect(body).to all(include('isOwner' => true))
+        end
       end
     end
   end
